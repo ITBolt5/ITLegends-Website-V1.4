e5d1f292b8d7699d5dfc3aa1ac267e8002227d38
ITBolt4 <bolt4@itlegends.co.za>
2025-11-14 22:29:11 +0200

    Updated package.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
+import ManagedITSupport from './pages/ManagedITSupport.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/services/managed-it-support" element={<ManagedITSupport />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
